280be589b0bfd111d4814f2e587465d8492f5d85
adortolan <adortolan@gmail.com>
2024-05-17 19:10:51 -0300

    implementando-o-caso-de-uso-de-listagem-de-categorias

---
+++ infrastructure/src/main/java/com/codeflixjava/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.codeflixjava.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.codeflixjava")
+public class WebServerConfig {
+}

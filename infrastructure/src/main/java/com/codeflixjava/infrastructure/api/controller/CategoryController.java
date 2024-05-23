272b2cb2e92d72d6fb37dca5f9aaf77c01d6e1ea
adortolan <adortolan@gmail.com>
2024-05-23 20:23:31 -0300

    criando-a-estrategia-para-testes-integrados-de-controller

--- infrastructure/src/main/java/com/codeflixjava/infrastructure/api/controller/CategoryController.java
+++ infrastructure/src/main/java/com/codeflixjava/infrastructure/api/controller/CategoryController.java
@@ -1,12 +1,21 @@
 package com.codeflixjava.infrastructure.api.controller;
 
+import com.codeflixjava.domain.category.CategoryGateway;
 import com.codeflixjava.domain.pagination.Pagination;
 import com.codeflixjava.infrastructure.api.CategoryAPI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class CategoryController implements CategoryAPI {
+
+    private final CategoryGateway categoryGateway;
+
+    public CategoryController(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
 
     @Override
     public ResponseEntity<?> createCategory() {

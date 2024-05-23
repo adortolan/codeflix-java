48bc94e1c5db7021b69b410659d134e288bda506
adortolan <adortolan@gmail.com>
2024-05-23 20:10:58 -0300

    incluindo-a-biblioteca-springdocs-openapi

--- infrastructure/src/main/java/com/codeflixjava/infrastructure/api/controller/CategoryController.java
+++ infrastructure/src/main/java/com/codeflixjava/infrastructure/api/controller/CategoryController.java
@@ -3,8 +3,11 @@ package com.codeflixjava.infrastructure.api.controller;
 import com.codeflixjava.domain.pagination.Pagination;
 import com.codeflixjava.infrastructure.api.CategoryAPI;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CategoryController implements CategoryAPI {
+
     @Override
     public ResponseEntity<?> createCategory() {
         return null;

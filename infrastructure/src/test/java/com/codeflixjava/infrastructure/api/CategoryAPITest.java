6e332caf7b78f98a1408efafd33d7845e752f37e
adortolan <adortolan@gmail.com>
2024-06-03 19:03:49 -0300

    testes-integrados-do-endpoint-de-delete-de-categoria-por-id

--- infrastructure/src/test/java/com/codeflixjava/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/codeflixjava/infrastructure/api/CategoryAPITest.java
@@ -3,6 +3,7 @@ import static io.vavr.API.Left;
 import static io.vavr.API.Right;
 
 import com.codeflixjava.application.category.create.CreateCategoryOutput;
+import com.codeflixjava.application.category.delete.DeleteCategoryUseCase;
 import com.codeflixjava.application.category.retrieve.get.CategoryOutput;
 import com.codeflixjava.application.category.retrieve.get.GetCategoryByIdUseCase;
 import com.codeflixjava.application.category.update.UpdateCategoryOutput;
@@ -52,6 +53,9 @@ public class CategoryAPITest {
 
     @MockBean
     private UpdateCategoryUseCase updateCategoryUseCase;
+
+    @MockBean
+    private DeleteCategoryUseCase deleteCategoryUseCase;
 
     @MockBean
     private GetCategoryByIdUseCase  getCategoryByIdUseCase;
@@ -313,5 +317,28 @@ public class CategoryAPITest {
                         && Objects.equals(expectedDescription, cmd.description())
                         && Objects.equals(expectedIsActive, cmd.isActive())
         ));
+    }
+
+    @Test
+    public void givenAValidId_whenCallsDeleteCategory_shouldBeOK() throws Exception {
+        // given
+        final var expectedId = "123";
+
+        doNothing()
+                .when(deleteCategoryUseCase).execute(any());
+
+        // when
+        final var request = delete("/categories/{id}", expectedId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        final var response = this.mvc.perform(request)
+                .andDo(print());
+
+        // then
+        response.andExpect(status().isNotFound())
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE));
+
+        verify(deleteCategoryUseCase, times(1)).execute(eq(expectedId));
     }
 }

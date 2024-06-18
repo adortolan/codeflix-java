64d9b49cf30b56c1a6df377bda9dd0109f0bfa9e
adortolan <adortolan@gmail.com>
2024-06-18 19:27:37 -0300

    testes-do-comportamento-de-adicionar-e-remover-uma-categoria

--- domain/src/main/java/com/codeflixjava/domain/genre/Genre.java
+++ domain/src/main/java/com/codeflixjava/domain/genre/Genre.java
@@ -86,7 +86,7 @@ public class Genre extends AggregateRoot<GenreID> {
             deactivate();
         }
         this.name = aName;
-        this.categories = new ArrayList<>(categories);
+        this.categories = new ArrayList<>(categories != null ? categories : Collections.emptyList());
         this.updatedAt = InstantUtils.now();
         selfValidate();
         return this;

164bf566122386d9cf30e49ef5761fbbd61e08b7
adortolan <adortolan@gmail.com>
2024-07-04 16:09:55 -0300

    implementando-o-metodo-create-do-gateway-de-genre

--- infrastructure/src/main/java/com/codeflixjava/infrastructure/genre/GenreMySQLGateway.java
+++ infrastructure/src/main/java/com/codeflixjava/infrastructure/genre/GenreMySQLGateway.java
@@ -5,13 +5,25 @@ import com.codeflixjava.domain.genre.GenreGateway;
 import com.codeflixjava.domain.genre.GenreID;
 import com.codeflixjava.domain.pagination.Pagination;
 import com.codeflixjava.domain.pagination.SearchQuery;
+import com.codeflixjava.infrastructure.genre.persistence.GenreJpaEntity;
+import com.codeflixjava.infrastructure.genre.persistence.GenreRepository;
+import org.springframework.stereotype.Component;
 
+import java.util.Objects;
 import java.util.Optional;
 
+@Component
 public class GenreMySQLGateway implements GenreGateway {
+
+    private final GenreRepository genreRepository;
+
+    public GenreMySQLGateway(final GenreRepository genreRepository) {
+        this.genreRepository = Objects.requireNonNull(genreRepository);
+    }
+
     @Override
     public Genre create(Genre aGenre) {
-        return null;
+        return save(aGenre);
     }
 
     @Override
@@ -32,5 +44,10 @@ public class GenreMySQLGateway implements GenreGateway {
     @Override
     public Pagination<Genre> findAll(SearchQuery aQuery) {
         return null;
+    }
+
+    private Genre save(final Genre aGenre) {
+        return this.genreRepository.save(GenreJpaEntity.from(aGenre))
+                .toAggregate();
     }
 }

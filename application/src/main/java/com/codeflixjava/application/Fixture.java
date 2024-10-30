d52480eafaa64af7a2123418df18fc0a66c44999
adortolan <adortolan@gmail.com>
2024-10-29 21:01:19 -0300

    testes-unitarios-do-caso-de-uso-de-criacao-de-castmember-com-fixtures

---
+++ application/src/main/java/com/codeflixjava/application/Fixture.java
@@ -0,0 +1,17 @@
+package com.codeflixjava.application;
+
+import com.codeflixjava.domain.castmember.CastMemberType;
+import com.github.javafaker.Faker;
+
+public final class Fixture {
+    private static final Faker FAKER = new Faker();
+    public static String name() {
+        return FAKER.name().fullName();
+    }
+    public static final class CastMember {
+        public static CastMemberType type() {
+            return FAKER.options()
+                    .option(CastMemberType.ACTOR, CastMemberType.DIRECTOR);
+        }
+    }
+}

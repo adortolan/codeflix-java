13263cf2a13f38ac1f055efa4b64d4d70c3bb2aa
adortolan <adortolan@gmail.com>
2024-11-09 10:49:46 -0300

    testes-integrados-do-endpoint-de-atualizacao-de-castmember

--- application/src/main/java/com/codeflixjava/application/castmember/update/UpdateCastMemberOutput.java
+++ application/src/main/java/com/codeflixjava/application/castmember/update/UpdateCastMemberOutput.java
@@ -1,9 +1,15 @@
 package com.codeflixjava.application.castmember.update;
 
 import com.codeflixjava.domain.castmember.CastMember;
+import com.codeflixjava.domain.castmember.CastMemberID;
 
 public record UpdateCastMemberOutput(String id) {
+
+    public static UpdateCastMemberOutput from(final CastMemberID anId) {
+        return new UpdateCastMemberOutput(anId.getValue());
+    }
+
     public static UpdateCastMemberOutput from(final CastMember aMember) {
-        return new UpdateCastMemberOutput(aMember.getId().getValue());
+        return from(aMember.getId());
     }
 }

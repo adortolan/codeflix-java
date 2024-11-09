a42c90351b64e9453b4759ea7833d009c114fe4b
adortolan <adortolan@gmail.com>
2024-11-09 11:38:34 -0300

    testes-de-desserializacao-da-classe-castmemberlistresponse

---
+++ infrastructure/src/test/java/com/codeflixjava/infrastructure/castmember/models/CastMemberListResponseTest.java
@@ -0,0 +1,34 @@
+package com.codeflixjava.infrastructure.castmember.models;
+
+import com.codeflixjava.Fixture;
+import com.codeflixjava.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+import java.time.Instant;
+
+@JacksonTest
+class CastMemberListResponseTest {
+    @Autowired
+    private JacksonTester<CastMemberListResponse> json;
+    @Test
+    public void testMarshall() throws Exception {
+        final var expectedId = "123";
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type().name();
+        final var expectedCreatedAt = Instant.now().toString();
+        final var response = new CastMemberListResponse(
+                expectedId,
+                expectedName,
+                expectedType,
+                expectedCreatedAt
+        );
+        final var actualJson = this.json.write(response);
+        Assertions.assertThat(actualJson)
+                .hasJsonPathValue("$.id", expectedId)
+                .hasJsonPathValue("$.name", expectedName)
+                .hasJsonPathValue("$.type", expectedType)
+                .hasJsonPathValue("$.created_at", expectedCreatedAt);
+    }
+}

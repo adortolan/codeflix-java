c841cc547f76ca27f2fe5e9e35883963aec5fbed
adortolan <adortolan@gmail.com>
2024-11-09 11:39:55 -0300

    testes-de-desserializacao-da-classe-createcastmemberequest

---
+++ infrastructure/src/test/java/com/codeflixjava/infrastructure/castmember/models/CreateCastMemberRequestTest.java
@@ -0,0 +1,29 @@
+package com.codeflixjava.infrastructure.castmember.models;
+
+import com.codeflixjava.JacksonTest;
+import com.codeflixjava.application.Fixture;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+import static org.junit.jupiter.api.Assertions.*;
+@JacksonTest
+class CreateCastMemberRequestTest {
+    @Autowired
+    private JacksonTester<CreateCastMemberRequest> json;
+    @Test
+    public void testUnmarshall() throws Exception {
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type();
+        final var json = """
+                {
+                  "name": "%s",
+                  "type": "%s"
+                }
+                """.formatted(expectedName, expectedType);
+        final var actualJson = this.json.parse(json);
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("type", expectedType);
+    }
+}

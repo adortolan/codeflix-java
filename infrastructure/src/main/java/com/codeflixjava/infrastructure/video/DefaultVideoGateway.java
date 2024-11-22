e1ba75a991c0a08d491b0d76ee14606757e503c3
adortolan <adortolan@gmail.com>
2024-11-22 17:28:28 -0300

    implementando-o-metodo-create-do-gateway-de-video

---
+++ infrastructure/src/main/java/com/codeflixjava/infrastructure/video/DefaultVideoGateway.java
@@ -0,0 +1,38 @@
+package com.codeflixjava.infrastructure.video;
+
+import com.codeflixjava.domain.pagination.Pagination;
+import com.codeflixjava.domain.video.*;
+import com.codeflixjava.infrastructure.video.persistence.VideoJpaEntity;
+import com.codeflixjava.infrastructure.video.persistence.VideoRepository;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Objects;
+import java.util.Optional;
+
+public class DefaultVideoGateway implements VideoGateway {
+    private final VideoRepository videoRepository;
+    public DefaultVideoGateway(final VideoRepository videoRepository) {
+        this.videoRepository = Objects.requireNonNull(videoRepository);
+    }
+    @Override
+    @Transactional
+    public Video create(final Video aVideo) {
+        return this.videoRepository.save(VideoJpaEntity.from(aVideo))
+                .toAggregate();
+    }
+    @Override
+    public void deleteById(VideoID anId) {
+    }
+    @Override
+    public Optional<Video> findById(VideoID anId) {
+        return Optional.empty();
+    }
+    @Override
+    public Video update(Video aVideo) {
+        return null;
+    }
+
+    @Override
+    public Pagination<VideoPreview> findAll(VideoSearchQuery aQuery) {
+        return null;
+    }
+}
